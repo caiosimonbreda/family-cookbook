037773cfba16d0482b5d8432cce3644063345fb8
Caio Brêda <caiosimonbreda@gmail.com>
2023-10-07 15:40:59 +0100

    path alias config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { fileURLToPath, URL } from "url";
+import path from "path"
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+      // "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+    ],
+  },
 })
