37a1cbc02dc3a8bd1780a063c2990ed2f7639712
Caio Brêda <caiosimonbreda@gmail.com>
2023-10-07 16:30:40 +0100

    send in json data as props to router comp

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
 import Navigation from './components/Navigation.vue'
+import recipes from './assets/recipes.json'
+import authors from './assets/authors/authors.json'
+import { ref } from 'vue'
+import { Recipe, Author } from '@/types/global'
+
+const recipeList = ref<Recipe[]>([])
+const authorsList = ref<Author[]>([])
+
+recipeList.value = recipes.recipes
+authorsList.value = authors.authors
+
 </script>
 
 <template>
   <Navigation>
     <RouterView v-slot="{ Component, route }">
       <transition name="fade" mode="out-in">
-        <component :is="Component" :key="route.path" />
+        <component :is="Component" :key="route.path" :recipes="recipeList" :authors="authorsList" />
       </transition>
     </RouterView>
   </Navigation>
