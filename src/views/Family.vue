de49660a53c49d7bda7932ce1699f8fa13743097
Caio Brêda <caiosimonbreda@gmail.com>
2023-12-08 00:22:14 +0000

    general info, signature

--- src/views/Family.vue
+++ src/views/Family.vue
@@ -40,12 +40,6 @@ onUnmounted(() => {
 
 const aFamilyMatter = ref<HTMLDivElement>()
 
-const scrollToSection = () => {
-  if (aFamilyMatter.value) {
-    aFamilyMatter.value.scrollIntoView({ behavior: 'smooth' });
-  }
-}
-
 </script>
 
 <template>
@@ -69,35 +63,51 @@ const scrollToSection = () => {
             class="flex flex-col w-full min-h-[calc(100vh-64px)] md:min-h-[calc(100vh-128px)] snap-start border-b-2 border-black">
             <h1 class="jumbotitle font-stinger p-9 pt-5">Roots that run deep</h1>
             <div class="flex flex-row w-full h-full items-end mb-20 md:mb-6">
-              <a @click="aFamilyMatter?.scrollIntoView({behavior: 'smooth'})" class="relative w-fit mx-auto cursor-pointer animate-bounce mt-4"><span
+              <a @click="aFamilyMatter?.scrollIntoView({ behavior: 'smooth' })"
+                class="relative w-fit mx-auto cursor-pointer animate-bounce mt-4"><span
                   class="material-symbols-outlined text-[4.5rem] md:text-[75px] font-light">
                   expand_more
                 </span></a>
             </div>
           </div>
           <div
-            class="flex flex-col gap-7 w-full xl:w-2/3 margin-auto min-h-[calc(100vh-64px)] md:min-h-[calc(100vh-128px)] snap-start p-12"
+            class="flex flex-col gap-7 w-full margin-auto min-h-[calc(100vh-64px)] md:min-h-[calc(100vh-128px)] snap-start p-12 overflow-scroll"
             id="a-family-matter" ref="aFamilyMatter">
             <h2 class="font-stinger text-[3.25rem] leading-[110%] font-bold mb-0.5 -mt-3">A Family Matter</h2>
-            <p class="hyphens-none text-xl md:text-lg leading-normal lg:leading-[1.7]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore
-              et dolore magna aliqua. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur. In
-              metus
-              vulputate eu scelerisque felis imperdiet proin fermentum. Parturient montes nascetur ridiculus
-              mus
-              mauris vitae ultricies leo integer. Semper viverra nam libero justo laoreet sit amet cursus.
-              Enim
-              neque volutpat ac tincidunt vitae semper quis lectus nulla.
+            <p class="hyphens-none text-xl md:text-lg leading-normal lg:leading-[1.7] xl:w-2/3">
+              On what may have been a beautiful day in the mid 1980s, my maternal grandmother picked up a plain-looking
+              yellow notebook at the supermarket during a grocery run at my aunt Claudia's request. My aunt, about 16
+              years old at the time, wanted to start noting down her mum's recipes.
             </p>
-            <p class="hyphens-none text-xl md:text-lg leading-normal lg:leading-[1.7]">
-              Quis vel eros donec ac. Magna fringilla urna porttitor rhoncus dolor purus non. Tincidunt ornare
-              massa eget egestas purus viverra accumsan in nisl. Feugiat in fermentum posuere urna nec
-              tincidunt.
-              Iaculis nunc sed augue lacus viverra vitae congue. Ornare quam viverra orci sagittis. Congue eu
-              consequat ac felis donec et.
+            <p class="hyphens-none text-xl md:text-lg leading-normal lg:leading-[1.7] xl:w-2/3">
+              The project caught traction, and adding recipes to the notebook became a collaborative effort between all
+              four sisters with recipes of their own and, eventually, my grandmother joined in herself (though her
+              handwriting wasn't nearly as neat. Sorry, gran). Recipes by family friends were also added, turning the
+              bright yellow notebook into a compendium of the best recipes that ever made their way into grandma's
+              kitchen.
             </p>
+            <p class="hyphens-none text-xl md:text-lg leading-normal lg:leading-[1.7] xl:w-2/3">
+              Through the course of its life, the yellow notebook got stained, torn and grimy, and each time it always got
+              re-bound, glued, and taped together. It lives to this day – each descending household owns a copy, and the
+              original rests safely in grandma's bedside drawer, away from the kitchen so as to avoid any further battle
+              scars.
+            </p>
+            <p class="hyphens-none text-xl md:text-lg leading-normal lg:leading-[1.7] xl:w-2/3">
+              With the passing of the years, the family has been growing larger, to which I've decided to create a digital
+              repository for the contents of the yellow notebook that anyone can access. I'll also update it over time
+              with new recipes that younger family members scavenge, create and perfect.
+            </p>
+            <p class="hyphens-none text-xl md:text-lg leading-normal lg:leading-[1.7] opacity-40 xl:w-2/3">
+              P.S.: You might notice an apparent lack of precision on the instructions in many of the entries. These
+              recipes were shared between people with a lot of years under their aprons – the recipes are a product of
+              their time and of the people who wrote them down, and I wouldn't dream of changing them.
+            </p>
+            <div class="flex justify-start">
+              <img class="flex w-1/2 md:w-5/12 lg:w-4/12 xl:w-3/12 mt-6" src="/src/assets/authors/signature.webp"
+                alt="Author's Signature: Caio Breda">
+            </div>
           </div>
+
         </section>
         <section ref="authorcarousel" @mouseover="isHoveringCarousel = true" @mouseout="isHoveringCarousel = false"
           class="flex flex-none flex-col w-full lg:w-3/12 border-t-2 lg:border-l-2 border-black lg:overflow-scroll scroll-smooth">
