335586a58bfdbaff9c8958a18b102f39b881e4e8
Caio Brêda <caiosimonbreda@gmail.com>
2023-12-13 21:44:22 +0000

    main title size adjustment, scroll fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,11 +17,11 @@ const goToRecipe = (recipe: typeof props.recipes[0]) => {
 
 <template>
     <main class="flex flex-col w-full h-full overflow-scroll snap-both snap-proximity scroll-smooth">
-        <section class="flex flex-col w-full min-h-[calc(100vh-64px)] md:min-h-[calc(100vh-128px)] overflow-scroll">
-            <h1 class="jumbotitle font-stinger leading-[85%] p-10 pt-5">The family recipes that nurtured
+        <section class="flex flex-col w-full min-h-[calc(100vh-64px)] md:min-h-[calc(100vh-128px)] overflow-hidden">
+            <h1 class="jumbotitle font-stinger leading-[85%] p-6 md:p-10 pt-5">The family recipes that nurtured
                 our
                 love of food</h1>
-            <div class="flex flex-row w-full h-full items-end mb-24 md:mb-0">
+            <div class="flex flex-row w-full h-full items-end md:mb-0 self-end">
                 <a href="#recipe-list
                 " class="relative w-fit mx-auto cursor-pointer animate-bounce mt-4"><span
                         class="material-symbols-outlined text-[60px] md:text-[80px] font-light">
@@ -30,7 +30,7 @@ const goToRecipe = (recipe: typeof props.recipes[0]) => {
             </div>
         </section>
         <section class="flex flex-row w-full" id="recipe-list">
-            <ol class="flex flex-col gap-6 font-jost p-10 pt-9 text-xl flex-wrap">
+            <ol class="flex flex-col gap-6 font-jost p-7 md:p-10 pt-9 text-xl flex-wrap">
                 <li class="hover:text-2xl hover:leading-[120%] transition-all duration-200 cursor-pointer"
                     v-for="recipe in recipes" @click="goToRecipe(recipe)">{{
                         `${recipe.id}. ${recipe.recipeName}` }}</li>
@@ -47,32 +47,38 @@ const goToRecipe = (recipe: typeof props.recipes[0]) => {
 
 @media screen and (max-width: 20000px) {
     .jumbotitle {
-        font-size: clamp(8em, 20.5vh, 80em);
+        font-size: clamp(8em, 22svh, 80em);
         line-height: 85%;
     }
 }
 
-@media screen and (max-width: 1400px) {
+@media screen and (max-width: 1440px) {
     .jumbotitle {
-        font-size: clamp(8em, 20vh, 80em);
+        font-size: clamp(8em, 19.5svh, 80em);
     }
 }
 
 @media screen and (max-width: 1200px) {
     .jumbotitle {
-        font-size: clamp(8em, 12vh, 40em);
+        font-size: clamp(8em, 12svh, 40em);
     }
 }
 
 @media screen and (max-width: 900px) {
     .jumbotitle {
-        font-size: clamp(3rem, 16vh, 40em);
+        font-size: clamp(3rem, 13svh, 40em);
     }
 }
 
 @media screen and (max-width: 600px) {
     .jumbotitle {
-        font-size: clamp(3rem, 12vh, 40em);
+        font-size: clamp(3rem, 11svh, 40em);
+    }
+}
+
+@media screen and (max-width: 360px) {
+    .jumbotitle {
+        font-size: clamp(3rem, 10.5svh, 40em);
     }
 }
 </style>
